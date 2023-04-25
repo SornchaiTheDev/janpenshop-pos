dd418945dfe33870371d563a46a432684e7cde2c
SornchaiTheDev <sornchaithedev@gmail.com>
2023-04-25 17:32:03 +0700

    Add : Prisma Context

--- renderer/src/server/context/createContext.ts
+++ renderer/src/server/context/createContext.ts
@@ -1,5 +1,11 @@
 import { CreateNextContextOptions } from '@trpc/server/adapters/next'
+import { inferAsyncReturnType } from '@trpc/server'
+import { prisma } from '../prisma'
 
-export default function createContext(opts: CreateNextContextOptions) {
-  
+export async function createContext(opts: CreateNextContextOptions) {
+  return {
+    prisma,
+  }
 }
+
+export type Context = inferAsyncReturnType<typeof createContext>
